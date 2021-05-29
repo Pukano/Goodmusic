4884522022dd985028458faa8ec1f062dff0c583
Pukano <pukano29@gmail.com>
2021-05-29 09:32:08 +0200

    12

--- good_music/settings.py
+++ good_music/settings.py
@@ -136,11 +136,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CommpressedManifestStaticFilesStorage'
 
 INTERNAL_IPS = ['127.0.0.1']
